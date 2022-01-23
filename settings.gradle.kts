0b82b492da543f2b8cc05fd2cf38a65c78231036
james-lee2 <james.lee2@workday.com>
2022-01-22 18:34:14 -0600

    Add gradle refreshVersions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("de.fayard.refreshVersions") version "0.30.2"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
