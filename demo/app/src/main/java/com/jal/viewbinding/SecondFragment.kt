25dca37c71234579dcf114939af34720e28aee27
james-lee2 <james.lee2@workday.com>
2022-01-25 23:57:18 -0600

    Add second fragment

---
+++ demo/app/src/main/java/com/jal/viewbinding/SecondFragment.kt
@@ -0,0 +1,23 @@
+package com.jal.viewbinding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jal.viewbinding.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private val viewBindingProvider by viewBindingProvider<FragmentSecondBinding>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return viewBindingProvider.initialize {
+            FragmentSecondBinding.inflate(inflater, container, false)
+        }
+    }
+}
